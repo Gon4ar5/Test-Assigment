33457a34d29e573f05d93a1ccca1bc81fb9fdfdc
Gon4ar5 <stas.gon4ar@gmail.com>
2023-05-31 19:03:36 +0300

    Add models and migrations

---
+++ db/migrate/20230531160010_create_experiments.rb
@@ -0,0 +1,11 @@
+class CreateExperiments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :experiments do |t|
+      t.text :name
+      t.text :value
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
