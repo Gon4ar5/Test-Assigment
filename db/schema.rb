84e2d596663601e30316dd95378e8624d4c895b4
Gon4ar5 <stas.gon4ar@gmail.com>
2023-06-01 15:16:00 +0300

    add docker-compose

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_31_160010) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "experiments", force: :cascade do |t|
     t.text "name"
     t.text "value"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_experiments_on_user_id"
